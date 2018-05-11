0e88b73fd75fcebaec95be72743c2e40045ac40d
PXTong <976988096@qq.com>
2018-05-11 17:33:08 +0800

    img

--- src/pages/check-accounts/index/components/child.vue
+++ src/pages/check-accounts/index/components/child.vue
@@ -6,24 +6,23 @@
         <li>
           <a class="" href="">
             <span>对账录入</span>
-            <span class="pull-right"><img src="~common/img/arrow.png" alt=""></span>
+            <span class="pull-right"><i class="i-img"></i></span>
           </a>
         </li>
         <li>
           <a class="" href="">
             <span>对账授权</span>
-            <span class="pull-right"><img src="~common/img/arrow.png" alt=""></span>
+            <span class="pull-right"><i class="i-img"></i></span>
           </a>
         </li>
         <li>
           <a class="" href="">
             <span>对账查询</span>
-            <span class="pull-right"><img src="~common/img/arrow.png" alt=""></span>
+            <span class="pull-right"><i class="i-img"></i></span>
           </a>
         </li>
       </ul>
     </div>
-
   </div>
 </template>
 
@@ -46,6 +45,14 @@
 <style lang="less" scoped>
   @black-gray:#333;
   @line-gray:#ddd;
+  .i-img{
+    background-image: url('~common/img/arrow.png');
+    background-size: 20px 30px;
+    background-repeat: no-repeat;
+    width: 30px;
+    height: 30px;
+    display: block;
+  }
   .container{
     overflow: hidden;
   }
