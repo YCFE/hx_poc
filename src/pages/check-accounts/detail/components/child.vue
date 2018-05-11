ce86e9bbceb4407522c2294cde3ae15a9bd0d4fe
PXTong <976988096@qq.com>
2018-05-11 21:42:25 +0800

    对账复核    待修改

--- src/pages/check-accounts/detail/components/child.vue
+++ src/pages/check-accounts/detail/components/child.vue
@@ -2,54 +2,54 @@
   <div class="container">
     <!-- Hello {{name}} -->
     <div class="account-detail">
-      <ul>
+      <ul v-for="(item,index) in options" :key="index">
         <li>
           <span class="span-label">账号</span>
-          <span class="span-content">11020110446967</span>
+          <span class="span-content">{{ item.accountNumber }}</span>
         </li>
         <li>
           <span class="span-label">币种</span>
-          <span class="span-content">人民币</span>
+          <span class="span-content">{{item.currency}}</span>
         </li>
         <li>
           <span class="span-label">账户名称</span>
-          <span class="span-content">华夏银行厦门分行**公司</span>
+          <span class="span-content">{{item.accountName}}</span>
         </li>
         <li>
           <span class="span-label">账户余额</span>
-          <span class="span-content">18,850,000</span>
+          <span class="span-content">{{item.accountBalance}}</span>
         </li>
         <li>
           <span class="span-label">余额日期</span>
-          <span class="span-content">2018-05-05</span>
+          <span class="span-content">{{item.date}}</span>
         </li>
         <li>
           <span class="span-label">余额状态</span>
-          <span class="span-content">余额相符</span>
+          <span class="span-content">{{item.state}}</span>
         </li>
         <li>
           <span class="span-label">发起人</span>
-          <span class="span-content">经办人01</span>
+          <span class="span-content">{{item.sponsor}}</span>
         </li>
         <li>
           <span class="span-label">授权人</span>
-          <span class="span-content">授权员01</span>
+          <span class="span-content">{{item.authorizer}}</span>
         </li>
         <li>
           <span class="span-label">调整相符</span>
-          <span class="span-content">否</span>
+          <span class="span-content">{{item.isMatch}}</span>
         </li>
         <li>
           <span class="span-label">调整相符原因</span>
-          <span class="span-content">无</span>
+          <span class="span-content">{{item.reason}}</span>
         </li>
         <li>
           <span class="span-label">余额调节表</span>
-          <span class="span-content"></span>
+          <span class="span-content">{{item.table}}</span>
         </li>
         <li>
           <span class="span-label">银行回复</span>
-          <span class="span-content"></span>
+          <span class="span-content">{{item.reply}}</span>
         </li>
       </ul>
     </div>
@@ -63,14 +63,27 @@
     name: 'child',
     data() {
       return {
-        name: 'child'
+        name: 'child',
+        options: [
+          {
+            accountNumber: '12312356',
+            currency: '人民币',
+            accountName: '李晓',
+            accountBalance: '12.00',
+            date: '2018-01-15',
+            state: '状态',
+            sponsor: '发起人',
+            authorizer: '授权人',
+            isMatch: '否',
+            reason: '原因',
+            table: '',
+            reply: ''
+          }
+        ]
       }
     },
     mounted() {
-      /* let h = document.documentElement.clientHeight - 8;
-      document.getElementsByTagName('body')[0].style.height = h+'px';
-      document.getElementsByTagName('body')[0].style.margin = 0;
-      document.getElementsByTagName('body')[0].style.backgroundColor = '#efeff4'; */
+
     }
   }
 
---
+++ src/pages/check-accounts/reconciliation/components/child.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <section class="list-wrap">
+      <ul style="position: relative" class="border-set" v-for="(item, index) in options" :key="index">
+        <li class="li-dis  special">
+          <span class="text-color title-special">账号</span>
+          <span>{{item.accountNumber}}</span>
+        </li>
+        <li class="position-set">
+          <span class="text-color">币种</span><span class="currency-distance">{{item.currency}}</span>
+        </li>
+        <li class="li-dis">
+          <span class="text-color title">账号名称</span>
+          <span>{{item.accountName}}</span>
+        </li>
+        <li class="li-dis">
+          <span class="text-color title">账户余额</span>
+          <span>{{item.accountBalance}}</span>
+        </li>
+        <li class="border_none li-dis">
+          <span class="text-color title">余额日期</span>
+          <span>{{item.date}}</span>
+        </li>
+        <li class="radio-item">
+          <mt-radio
+            v-model="item.state"
+            :options="['同意授权', '拒绝授权']">
+          </mt-radio>
+        </li>
+        <li>
+          <input class="reason-input" type="text" v-if="item.state === '拒绝授权'" placeholder="拒绝授权原因">
+        </li>
+      </ul>
+    </section>
+    <p class="page-info">
+       共12条记录，显示7至9条记录
+    </p>
+    <!-- <div class="page-count clearfix">
+      <span class="fl">
+        <a href="javascript:;">上一页</a>/<a href="javascript:;">第一页</a>
+      </span>
+      <span class="fr">
+        <a href="javascript:;">下一页</a>/<a href="javascript:;">最后一页</a>
+      </span>
+    </div> -->
+    <div class="input-button">
+      <button class="btn btn-primary">下一步</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'child',
+    data() {
+      return {
+        name: 'child',
+        options:[
+          { accountNumber: '12312356', currency: '人民币', accountName: '李晓', accountBalance: '12.00', date: '2018-01-15', state: '' },
+          { accountNumber: '12312356', currency: '人民币', accountName: '李晓', accountBalance: '12.00', date: '2018-01-15', state: '' },
+          { accountNumber: '12312356', currency: '人民币', accountName: '李晓', accountBalance: '12.00', date: '2018-01-15', state: '' },
+        ],
+      }
+    },
+    mounted() {
+
+    }
+  }
+
+</script>
+
+<style lang="less">
+  .page-info{
+    margin: 30px 28px 64px;
+    color: #a1a1a1;
+    font-size: 28px;
+  }
+  .mint-cell{
+    background: none;
+  }
+  .input-button{
+    margin: 0 30px;
+  }
+  .list-wrap{
+    font-size: 30px;
+  }
+  .li-dis{
+    span:first-child{
+      width: 180px;
+    }
+  }
+</style>
