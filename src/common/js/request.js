d6b0556cb0a57c337c566da9f463c2ed71b9b1dc
dreamjser <dreamjser@gmail.com>
2018-05-14 16:23:34 +0800

    loading

--- src/common/js/request.js
+++ src/common/js/request.js
@@ -1,7 +1,12 @@
 export default (operationType, cb) => {
+  AlipayJSBridge.call('showLoading', {
+    text: '加载中'
+  });
+
   AlipayJSBridge.call('rpc', {
     operationType,
   }, r => {
+    AlipayJSBridge.call('hideLoading');
     cb && cb(r);
   });
 }
