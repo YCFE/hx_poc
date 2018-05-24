8c1a1ba9e05b434ef7e0570a529ed5396df8aa34
PXTong <976988096@qq.com>
2018-05-24 14:19:09 +0800

    check-accounts 文件新建  增加mock-mac运行命令

---
+++ src/pages/check-accounts/detail/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import mixins from '@/libs/mixins';
+
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  mixins: [mixins],
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="less">
+@import '~@/assets/less/base.less';
+</style>
---
+++ src/pages/check-accounts/index/app.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container" id="app">
+    <div class="index-list">
+      <ul>
+        <li>
+          <div class="a" @click="inputSearch">
+            <span>对账录入</span>
+            <span class="pull-right"><i class="i-img"></i></span>
+          </div>
+        </li>
+        <li>
+          <div class="a" href="" @click="reconciliation">
+            <span>对账授权</span>
+            <span class="pull-right"><i class="i-img"></i></span>
+          </div>
+        </li>
+        <li>
+          <div class="a" @click="search">
+            <span>对账查询</span>
+            <span class="pull-right"><i class="i-img"></i></span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import mixins from '@/libs/mixins';
+
+export default {
+  name: 'accountIndex',
+  data() {
+    return {
+
+    }
+  },
+  mixins: [mixins],
+  methods:{
+    inputSearch(){
+      AlipayJSBridge.call('pushWindow', {
+        url: 'input-search.html'
+      });
+    },
+    reconciliation(){
+      AlipayJSBridge.call('pushWindow', {
+        url: 'reconciliation.html'
+      });
+    },
+    search(){
+      AlipayJSBridge.call('pushWindow', {
+        url: 'search.html'
+      });
+    },
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+ @import '~@/assets/less/base.less';
+
+  @black-gray:#333;
+  @line-gray:#ddd;
+  .i-img{
+    background-image: url('~@/assets/img/arrow.png');
+    background-size: 20px 30px;
+    background-repeat: no-repeat;
+    width: 30px;
+    height: 30px;
+    display: block;
+  }
+  .container{
+    overflow: hidden;
+  }
+  .pull-right{
+    float: right;
+  }
+  .pull-left{
+    float: left;
+  }
+  ul,li{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  a{
+    text-decoration: none;
+  }
+  .index-list{
+    font-size: 30px;
+    margin: 0 29px;
+    .a{
+      padding: 45px 0 45px;
+      color: @black-gray;
+      display: block;
+      border-bottom: 1px solid @line-gray;
+      img{
+        height: 30px;
+      }
+    }
+  }
+</style>
+
