dbff427ae2add8f76e74faabfa7d3ae5333479be
dreamjser <dreamjser@gmail.com>
2018-06-13 10:21:35 +0800

    封装修改

--- src/libs/request.js
+++ src/libs/request.js
@@ -1,22 +1,14 @@
-const ready = (callback) => {
-  if (window.AlipayJSBridge) {
-    callback && callback();
-  } else {
-    document.addEventListener('AlipayJSBridgeReady', callback, false);
-  }
-}
+import briage from '@/libs/briage';
 
 export default (operationType, cb) => {
-  ready(() => {
-    AlipayJSBridge.call('showLoading', {
-      text: '加载中'
-    });
+  briage('showLoading', {
+    text: '加载中'
+  });
 
-    AlipayJSBridge.call('rpc', {
-      operationType,
-    }, r => {
-      AlipayJSBridge.call('hideLoading');
-      cb && cb(r);
-    });
+  briage('rpc', {
+    operationType,
+  }, r => {
+    briage('hideLoading');
+    cb && cb(r);
   });
 }
